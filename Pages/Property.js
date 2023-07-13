bb4a05886ff3304393f4b6a4947e6c01922b83b2
Gizem Şangür <gizem2002sangur@gmail.com>
2023-07-13 16:19:09 +0300

    Add icons for property part

--- Pages/Property.js
+++ Pages/Property.js
@@ -1,11 +1,19 @@
 import ReButton from "@/components/ReButton";
 import { Grid } from "@mui/material";
 import React from "react";
-
+import BusinessIcon from "@mui/icons-material/Business";
+import Apartment from "@/components/Apartment";
+import OfficeBuilding from "@/components/OfficeBuilding";
+import Duplex from "@/components/Duplex";
+import StripMall from "@/components/StripMall";
+import Industrial from "@/components/Industrial";
+import SingleFHouse from "@/components/SingleFHouse";
+import Trailerpark from "@/components/Trailerpark";
+import Vacation from "@/components/Vacation";
 export default function Property() {
   return (
     <Grid container dirction="row">
-      <Grid item md={4} sm={12} >
+      <Grid item md={4} sm={12}>
         <Grid>
           <b style={{ fontSize: "45px" }}>
             We do our best <br /> facilities provide you <br />
@@ -23,7 +31,11 @@ export default function Property() {
         <Grid
           container
           direction="row"
-          sx={{ justifyContent: "space-between", marginBottom: "40px",marginLeft:"40px" }}
+          sx={{
+            justifyContent: "space-between",
+            marginBottom: "40px",
+            marginLeft: "40px",
+          }}
         >
           <Grid item>
             <div
@@ -31,10 +43,15 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                fontSize: "18px",
+                textAlign: "center", 
               }}
             >
-              A
+              <Grid sx={{position:"relative",top:"20%"}}>
+                <Apartment/>
+                <p>Apartment</p>
+              </Grid>
             </div>
           </Grid>
           <Grid item>
@@ -43,10 +60,16 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                backgroundColor:"#46ACFF",
+                textAlign: "center", 
+                color:"white"
               }}
             >
-              A
+               <Grid sx={{position:"relative",top:"20%"}}>
+                <OfficeBuilding/>
+                <p>Office Building</p>
+              </Grid>
             </div>
           </Grid>
           <Grid item>
@@ -55,10 +78,14 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                textAlign: "center", 
               }}
             >
-              A
+              <Grid sx={{position:"relative",top:"20%"}}>
+                <Duplex/>
+                <p>Duplex</p>
+              </Grid>
             </div>
           </Grid>
           <Grid item>
@@ -67,10 +94,14 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                textAlign: "center",
               }}
             >
-              A
+               <Grid sx={{position:"relative",top:"20%"}}>
+                <StripMall/>
+                <p>StripMall</p>
+              </Grid>
             </div>
           </Grid>
         </Grid>
@@ -85,10 +116,14 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                textAlign: "center",
               }}
             >
-              A
+              <Grid sx={{position:"relative",top:"20%"}}>
+                <Industrial/>
+                <p>Industrial</p>
+              </Grid>
             </div>
           </Grid>
           <Grid item>
@@ -97,10 +132,14 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                textAlign: "center",
               }}
             >
-              A
+                <Grid sx={{position:"relative",top:"20%"}}>
+                <SingleFHouse/>
+                <p>Single Family House</p>
+              </Grid>
             </div>
           </Grid>
           <Grid item>
@@ -109,10 +148,14 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                textAlign: "center",
               }}
             >
-              A
+              <Grid sx={{position:"relative",top:"20%"}}>
+                <Trailerpark/>
+                <p>Trailer Park</p>
+              </Grid>
             </div>
           </Grid>
           <Grid item>
@@ -121,10 +164,14 @@ export default function Property() {
                 borderRadius: "8px",
                 width: "193px",
                 height: "173px",
-                border: "2px solid black",
+                border: "2px solid #E8E8E8",
+                textAlign: "center",
               }}
             >
-              A
+              <Grid sx={{position:"relative",top:"20%"}}>
+                <Vacation/>
+                <p>Vacation Rental</p>
+              </Grid>
             </div>
           </Grid>
         </Grid>
---
+++ components/OfficeBuilding.js
@@ -0,0 +1,268 @@
+import React from "react";
+
+export default function OfficeBuilding() {
+  return (
+    <div  style={{display:"flex",justifyContent:"center"}}>
+      <svg
+        width="58"
+        height="62"
+        viewBox="0 0 58 62"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.00128 61.0002H56.8248"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M36.4932 8.09668H5.50319V60.9918H36.4932V8.09668Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M34.3211 3.83716H7.68217V8.09672H34.3211V3.83716Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 13.7207H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 19.3196H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 24.927H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 30.5259H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 36.1333H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 41.7323H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M9.79616 47.3396H32.2004"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M36.4922 10.5261H52.3241V61.0002"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M29.114 51.9652H12.8744V61.0001H29.114V51.9652Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M21.0021 51.9652V61.0001"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.0865 10.526V1.00024"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M43.9391 10.5262V5.052"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 16.1084V18.6375"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 16.1084V18.6375"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 16.1084V18.6375"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 22.6892V25.2183"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 22.6892V25.2183"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 22.6892V25.2183"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 29.2615V31.7906"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 29.2615V31.7906"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 29.2615V31.7906"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 35.842V38.3712"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 35.842V38.3712"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 35.842V38.3712"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 42.4229V44.952"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 42.4229V44.952"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 42.4229V44.952"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 48.9952V51.5244"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 48.9952V51.5244"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 48.9952V51.5244"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M40.1612 55.5759V58.105"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M44.3532 55.5759V58.105"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M48.5471 55.5759V58.105"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
