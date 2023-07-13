31b8a9a653988a4cf14cdc899526ef2681a26755
Gizem Şangür <gizem2002sangur@gmail.com>
2023-07-13 13:48:01 +0300

    Make header and add to main page

---
+++ components/MenuB.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function MenuB() {
+  return (
+    <div>
+      <header style={{}}>
+        <nav>
+          <div>
+            <img src="./logo.png" />
+          </div>
+          <div>
+            <button>Home</button>
+            <button>Home</button>
+            <button>Home</button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
---
+++ components/MenuBar.js
@@ -0,0 +1,116 @@
+"use client";
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import LoginIcon from "@mui/icons-material/Login";
+const drawerWidth = 240;
+const navItems = [
+  "Home",
+  "How It Works",
+  "Features",
+  "Pricing",
+  "Login",
+  "Get Started Free",
+];
+export default function MenuBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex", backgroundColor: "white" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        sx={{ backgroundColor: "white", alignItems: "center" }}
+      >
+        <Toolbar
+          sx={{
+            width: "100%",
+            justifyContent: "space-between",
+            padding: "5vh",
+          }}
+        >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" }, color: "black" }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box>
+            <img src="./logo.png" />
+          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Button sx={{ color: "black",m:2 }}>Home</Button>
+            <Button sx={{ color: "black",m:2  }}>How It Works</Button>
+            <Button sx={{ color: "black",m:2  }}>Features</Button>
+            <Button sx={{ color: "black",m:2  }}>Pricing</Button>
+            <span style={{margin:"10px", padding:"10px"}}>
+              <LoginIcon sx={{color:"#5663DA"}} />
+              <Button sx={{ color: "black" }}>Login</Button>
+            </span>
+
+            <button  style={{ color: "white" ,backgroundColor:"#5663da",borderRadius:"35px",width:"195px",height:"50px"}} >Get Started Free</button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
